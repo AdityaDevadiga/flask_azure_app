fe1112f0ccabdf8f163105047015eaee75765d37
AYAN BHATTACHARYA <36237186+gitayanbhat@users.noreply.github.com>
2023-06-01 18:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/',methods = ['POST','GET'])
-
 def calculate():
     bmi = ''
     if request.method=='POST' and 'weight' in request.form and 'height' in request.form:
